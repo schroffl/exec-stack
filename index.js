d81ca84a3b9804ec4bef3b356b02743039a5de8e
Hannes <Hannes.Holzwarth@me.com>
2016-02-23 21:09:03 +0100

    Promise resolves with passed arguments
    + Added the ability to set the middleware context via .context()

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ class ExecStack {
 
     constructor() {
         this._stack = [];
+        this._context = {};
+    }
+
+    /**
+     * Set the context to be applied to middleware
+     * 
+     * @author schroffl
+     *
+     * @param {Object} [context={}] - The context to be applied to middleware
+     */
+
+    context(context) {
+        // Clear to context to an empty object if the parameter is ommitted
+        this._context = typeof context === 'object' ? context : {};
+        return this;
     }
 
     /**
@@ -63,44 +78,39 @@ class ExecStack {
         const args = Array.from(arguments),
               controller = new EventEmitter();
 
-        let event = typeof args[0] === 'function' ? ANY_EVENT : args.shift(),
-            resolve = null, 
-            reject = null,
-            promise = new Promise((res, rej) => {
-                resolve = res;
-                reject = rej;
+        let event = typeof args[0] === 'function' ? ANY_EVENT : args.shift();
+
+        return new Promise((resolve, reject) => {
+            controller.on('next', i => {
+                let current = this._stack[i];
+
+                // If i exceeds the amount of functions on the stack and none has thrown => resolve
+                if(i > this._stack.length)
+                    return resolve({ event, 'arguments': args.slice() });
+                // current is undefined ?! => continue
+                else if(!current)
+                    return controller.emit('next', i + 1);
+                // If the middleware is neither listening for all events nor the specified one => continue
+                else if(current.event !== ANY_EVENT && current.event !== event)
+                    return controller.emit('next', i + 1);
+
+                let passArgs = args.slice();
+
+                // If the middleware is listening for all events tell it what it's getting notified about
+                if(current.event === ANY_EVENT) passArgs.unshift(event);
+
+                const next = () => controller.emit('next', i + 1);
+                next.throw = err => reject(err);
+
+                // Push the "next" function
+                passArgs.push(next);
+
+                current.callback.apply(this._context, passArgs);
             });
 
-        controller.on('next', i => {
-            let current = this._stack[i];
-
-            // If i exceeds the amount of functions on the stack => abort
-            if(i > this._stack.length)
-                return resolve();
-            else if(!current)
-                return controller.emit('next', i + 1);
-            // If the middleware is neither listening for all events nor the specified one => continue
-            else if(current.event !== ANY_EVENT && current.event !== event)
-                return controller.emit('next', i + 1);
-
-            let passArgs = args.slice();
-
-            // If the middleware is listening for all events tell it what it's getting notified about
-            if(current.event === ANY_EVENT) passArgs.unshift(event);
-
-            const next = () => controller.emit('next', i + 1);
-            next.throw = err => reject(err);
-
-            // Push the "next" function
-            passArgs.push(next);
-
-            current.callback.apply({}, passArgs);
+            // Start the snowball
+            controller.emit('next', 0);
         });
-
-        // Start the snowball
-        controller.emit('next', 0);
-
-        return promise;
     }
 }
 
